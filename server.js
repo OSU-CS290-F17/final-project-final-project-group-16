5ede68fd5cb48b77b44b15997fcde2008013d83e
pingnomw <pingnomw@oregonstate.edu>
2017-11-27 17:18:34 -0800

    server.js now utilizes express instead of built-in modules added package.json containing dependencies and list of contributors

--- server.js
+++ server.js
@@ -1,14 +1,46 @@
 var http = require('http');
+console.log("Starting server...\n")
+
 var fs = require('fs');	
-var indexHTMLContent = fs.readFileSync('public/index.html', 'utf8');
-var indexJSContent = fs.readFileSync('public/index.js', 'utf8');
-var styleContent = fs.readFileSync('public/style.css', 'utf8');
+//var indexHTMLContent = fs.readFileSync('public/index.html', 'utf8');
+//var indexJSContent = fs.readFileSync('public/index.js', 'utf8');
+//var styleContent = fs.readFileSync('public/style.css', 'utf8');
+var express = require("express")
+var app = express()
+var publicDir = {root: __dirname + "/public/"}
+var error404page = "" // the filename of the 404 page located in /public directory
 
 var port = 3000;
 if(process.env.PORT!=null){
-  port = process.env.PORT;
+	port = process.env.PORT;
 }
 
+app.get("/", function (req, res){
+	res.status(200)
+	res.sendFile("index.html", publicDir, function(err){
+		if (err){
+			throw(err)
+		}
+	})
+})
+
+app.use(express.static('public'));
+
+app.use('*', function (req, res) {
+	res.status(404)
+	res.sendFile(error404page, publicDir, function(err){
+		if (err){
+			throw(err)
+		}
+	})
+});
+
+app.listen(port, function () {
+	console.log("Server started, listening on port", port);
+});
+
+
+/*
 var server = http.createServer(function (req, res) {
   console.log("== Request made");
   console.log("  - method:", req.method);
@@ -17,7 +49,7 @@ var server = http.createServer(function (req, res) {
   console.log("");
   
   console.log("== MY_ENV_VARIABLE:", process.env.PORT);
-   
+
   if(req.url == "/index.html"){
 	res.writeHead(200, {
 	  "Content-Type": "text/html"
@@ -50,3 +82,4 @@ var server = http.createServer(function (req, res) {
 server.listen(port, function () {
   console.log("== Server listening on port:", port);
 });
+*/
