043c05e202c1288553d496824190323f6890d3fd
bolyb <bolyb@oregonstate.edu>
2017-11-29 15:50:23 -0800

    Adding initial mongo db structure

--- server.js
+++ server.js
@@ -2,18 +2,26 @@ var http = require('http');
 console.log("Starting server...\n")
 
 var fs = require('fs');	
-//var indexHTMLContent = fs.readFileSync('public/index.html', 'utf8');
-//var indexJSContent = fs.readFileSync('public/index.js', 'utf8');
-//var styleContent = fs.readFileSync('public/style.css', 'utf8');
 var express = require("express")
 var app = express()
 var publicDir = {root: __dirname + "/public/"}
 var error404page = "" // the filename of the 404 page located in /public directory
+var MongoClient = require('mongodb').MongoClient;
 
 var port = 3000;
 if(process.env.PORT!=null){
 	port = process.env.PORT;
 }
+
+var mongoHost = process.env.MONGO_HOST;
+var mongoPort = process.env.MONGO_PORT || 27017;
+var mongoUser = process.env.MONGO_USER;
+var mongoPassword = process.env.MONGO_PASSWORD;
+var mongoDBName = process.env.MONGO_DB;
+var mongoURL = 'mongodb://' + mongoUser + ':' + mongoPassword +
+  '@' + mongoHost + ':' + mongoPort + '/' + mongoDBName;
+  
+ var mongoConnection = null;
 
 app.get("/", function (req, res){
 	res.status(200)
@@ -35,51 +43,12 @@ app.use('*', function (req, res) {
 	})
 });
 
-app.listen(port, function () {
-	console.log("Server started, listening on port", port);
+MongoClient.connect(mongoURL, function(err, connection) ) {
+	if (err) {
+		throw err;
+	}
+	mongoConnection = connection;
+	app.listen(port, function () {
+		console.log("Server started, listening on port", port);
+	});
 });
-
-
-/*
-var server = http.createServer(function (req, res) {
-  console.log("== Request made");
-  console.log("  - method:", req.method);
-  console.log("  - path:", req.url);
-  console.log("  - headers:", req.headers);
-  console.log("");
-  
-  console.log("== MY_ENV_VARIABLE:", process.env.PORT);
-
-  if(req.url == "/index.html"){
-	res.writeHead(200, {
-	  "Content-Type": "text/html"
-    });
-	res.write(indexHTMLContent);
-  }
-  else if(req.url == "/index.js"){
-	res.writeHead(200, {
-	  "Content-Type": "text/javascript"
-    });
-	res.write(indexJSContent);
-  } 
-  else if(req.url == "/style.css"){
-	res.writeHead(200, {
-	  "Content-Type": "text/css"
-    });
-	res.write(styleContent);
-  }
-  else if(req.url == "/"){
-	res.writeHead(200, {
-	  "Content-Type": "text/html"
-    });
-	res.write(indexHTMLContent);
-  }
-
-  res.end();
-
-});
-
-server.listen(port, function () {
-  console.log("== Server listening on port:", port);
-});
-*/
